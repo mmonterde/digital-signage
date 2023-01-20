6ce29a699a3c2e9f00370486385f1692d145ca18
Moises Monterde <moises@alquimiai.com>
2023-01-20 13:52:16 -0600

    error cannot set headers

--- api/helpers/widget_helper.js
+++ api/helpers/widget_helper.js
@@ -3,37 +3,40 @@ const CommonHelper = require('./common_helper')
 const console = require('console')
 
 function addWidget(req, res) {
-  let widget = req.crudify.result
-  console.log(widget)
-  return Display.findById(widget.display)
-    .then(display => {
-      if (!display) return res.status(404).json({ error: 'Display not found' })
-      display.widgets.push(widget._id)
-      return display.save().then(display => {
-        if (!display) return res.status(500).json({ error: 'Display not saved' })
-        return res.json({ success: true })
-      })
-    })
-    .catch(err => res.json(err))
+    let widget = req.crudify.result
+    return Display.findById(widget.display)
+        .then(display => {
+            if (display)
+                console.log(display)
+            display.widgets.push(widget._id)
+            return display.save().then(display => {
+                if (display)
+                    return res.json({ success: true })
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+
 }
 
 function deleteWidget(req, res) {
-  let widget = req.crudify.result
-  return Display.findById(widget.display).then(display => {
-    if (!display) return res.status(404).json({ error: 'Display not found' })
-    display.widgets = display.widgets.filter(function(value) {
-      return !widget._id.equals(value)
+    let widget = req.crudify.result
+    return Display.findById(widget.display).then(display => {
+        if (!display) return res.status(404).json({ error: 'Display not found' })
+        display.widgets = display.widgets.filter(function (value) {
+            return !widget._id.equals(value)
+        })
+        return display
+            .save()
+            .then(() => CommonHelper.broadcastUpdate(res.io))
+            .then(() => {
+                return res.json({ success: true })
+            })
     })
-    return display
-      .save()
-      .then(() => CommonHelper.broadcastUpdate(res.io))
-      .then(() => {
-        return res.json({ success: true })
-      })
-  })
 }
 
 module.exports = {
-  deleteWidget,
-  addWidget
+    deleteWidget,
+    addWidget
 }
